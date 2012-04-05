dae3a25182efb8762e36e7a2a8fe0f39b7d7ecd3
 沈 耕 <shintagaya@bogon.(none)>
2012-04-06 02:20:47 +0800

    Initial Commit

---
+++ to pi/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  to pi
+//
+//  Created by 耕  沈 on 12-4-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
